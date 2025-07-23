067f0fad4791d9bc355db2e1ef6032ac778f758a
Szymon Zyguła <szymon@zygula.eu>
2025-07-23 18:22:16 +0200

    Add data to union find

--- src/union_find.rs
+++ src/union_find.rs
@@ -1,95 +1,118 @@
-use std::cell::{Cell, RefCell};
+use std::{
+    cell::{Cell, RefCell},
+    collections::HashMap,
+    fmt::Debug,
+    rc::Rc,
+};
 
 type SetId = usize;
+type DataId = usize;
+
+trait UnionData: Debug {
+    fn merge(&mut self, other: Self);
+}
+
+#[derive(Debug)]
+enum UnionFindNode {
+    Data(DataId),
+    Pointer(Cell<SetId>),
+}
 
 // TODO: make two union finds: compressing and not notpressing?
 #[derive(Debug, Default)]
-pub struct UnionFind {
-    parents: Vec<Cell<SetId>>,
+pub struct UnionFind<D: UnionData> {
+    data: HashMap<DataId, D>,
+    contents: Vec<UnionFindNode>,
 }
 
-impl UnionFind {
-    pub fn with_size(size: usize) -> Self {
-        let mut vec = Vec::with_capacity(size);
+impl<D: UnionData> UnionFind<D> {
+    pub fn with_size(size: usize, mut generator: impl FnMut(usize) -> D) -> Self {
+        let mut data = HashMap::with_capacity(size);
+        let mut contents = Vec::with_capacity(size);
 
         for i in 0..size {
-            vec.push(Cell::new(i));
+            data.insert(i, generator(i));
+            contents.push(UnionFindNode::Data(i));
         }
 
-        Self { parents: vec }
+        Self { data, contents }
     }
 
     pub fn size(&self) -> usize {
-        self.parents.len()
+        self.contents.len()
     }
 
-    fn add(&mut self) -> SetId {
+    fn add(&mut self, data: D) -> SetId {
         let new_id = self.size();
-        self.parents.push(Cell::new(new_id));
+        self.data.insert(new_id, data);
+        self.contents.push(UnionFindNode::Data(new_id));
         new_id
     }
 
     fn parent(&self, id: SetId) -> SetId {
-        self.parents[id].get()
+        match &self.contents[id] {
+            UnionFindNode::Data(_) => id,
+            UnionFindNode::Pointer(cell) => cell.get(),
+        }
     }
 
     /// Merges set with ID `id_1` with set with ID `id_2`. The new canonical ID is `id_2`.
     fn union(&mut self, id_1: SetId, id_2: SetId) {
-        let id_1_canon = self.find(id_1);
-        let id_2_canon = self.find(id_2);
-        self.parents[id_1_canon] = Cell::new(id_2_canon);
-    }
+        let (id_1_canon, data_1_id) = self.find(id_1);
+        let (id_2_canon, data_2_id) = self.find(id_2);
 
-    fn find_no_compress(&self, id: SetId) -> SetId {
-        let parent = self.parent(id);
-        if parent == id {
-            return id;
+        if id_1_canon == id_2_canon {
+            // Nothing to do, both ids point to the same data
+            return;
         }
 
-        self.find_no_compress(parent)
+        let data_1 = self.data.remove(&data_1_id).unwrap();
+        self.data.get_mut(&data_2_id).unwrap().merge(data_1);
+
+        self.contents[id_1_canon] = UnionFindNode::Pointer(Cell::new(id_2_canon));
     }
 
-    fn find(&self, id: SetId) -> SetId {
-        let parent = self.parent(id);
-        if parent == id {
-            return id;
+    fn find(&self, id: SetId) -> (SetId, DataId) {
+        match &self.contents[id] {
+            UnionFindNode::Data(data_id) => (id, *data_id),
+            UnionFindNode::Pointer(parent) => {
+                let grandparent = self.parent(parent.get());
+                parent.set(grandparent);
+
+                self.find(parent.get())
+            }
         }
-
-        let grandparent = self.parent(parent);
-        self.parents[id].set(grandparent);
-
-        self.find(parent)
     }
 }
 
 /// A reference to a Union-Find set which always returns the canonical version of its set and
 /// can be used to interact with other sets.
 #[derive(Clone, Debug)]
-struct Set<'u> {
+pub struct Set<D: UnionData> {
     set_id: Cell<SetId>,
-    union_find: &'u RefCell<UnionFind>,
+    union_find: Rc<RefCell<UnionFind<D>>>,
 }
 
-impl<'u> Set<'u> {
-    pub fn new(union_find: &'u RefCell<UnionFind>) -> Self {
-        let set_id = union_find.borrow_mut().add();
+impl<D: UnionData> Set<D> {
+    pub fn new(union_find: &Rc<RefCell<UnionFind<D>>>, data: D) -> Self {
+        let set_id = union_find.borrow_mut().add(data);
 
         Self {
             set_id: Cell::new(set_id),
-            union_find,
+            union_find: Rc::clone(union_find),
         }
     }
 
     fn get(&self) -> SetId {
         // Update the cached id with the canonical one before returning it
-        let canonical_id = self.union_find.borrow().find(self.set_id.get());
+        let (canonical_id, _) = self.union_find.borrow().find(self.set_id.get());
         self.set_id.set(canonical_id);
         canonical_id
     }
 
-    pub fn union(self, other: Set<'u>) -> Set<'u> {
+    pub fn union(self, other: Set<D>) -> Set<D> {
         assert!(
-            std::ptr::eq(self.union_find, other.union_find),
+            std::ptr::eq(self.union_find.as_ref(), other.union_find.as_ref()),
             "Sets on which a union is performed have to belong to the same union-find."
         );
 
@@ -101,28 +124,42 @@ impl<'u> Set<'u> {
     }
 }
 
-impl<'u> PartialEq for Set<'u> {
+impl<D: UnionData> PartialEq for Set<D> {
     fn eq(&self, other: &Self) -> bool {
         self.get() == other.get()
     }
 }
 
-impl<'u> Eq for Set<'u> {}
+impl<D: UnionData> Eq for Set<D> {}
+
+impl UnionData for () {
+    fn merge(&mut self, _: Self) {}
+}
 
 #[cfg(test)]
 mod tests {
-    use std::cell::RefCell;
+    use std::{cell::RefCell, rc::Rc};
 
     #[test]
-    fn creation() {
-        let uf = super::UnionFind::with_size(10);
+    fn creation_with_addition() {
+        let mut uf = super::UnionFind::<()>::default();
+        uf.add(());
+        uf.add(());
+        uf.add(());
+
+        assert_eq!(uf.size(), 3)
+    }
+
+    #[test]
+    fn creation_with_size() {
+        let uf = super::UnionFind::with_size(10, |_| ());
 
         assert_eq!(uf.size(), 10)
     }
 
     #[test]
     fn union_and_find() {
-        let mut uf = super::UnionFind::with_size(4);
+        let mut uf = super::UnionFind::with_size(4, |_| ());
         uf.union(0, 2);
         uf.union(3, 1);
         uf.union(1, 0);
@@ -134,8 +171,8 @@ mod tests {
 
     #[test]
     fn add_set() {
-        let mut uf = super::UnionFind::with_size(1);
-        let new_id = uf.add();
+        let mut uf = super::UnionFind::with_size(1, |_| ());
+        let new_id = uf.add(());
 
         assert_eq!(new_id, 1);
         assert_eq!(uf.size(), 2);
@@ -143,21 +180,29 @@ mod tests {
 
     #[test]
     fn compression() {
-        let mut uf = super::UnionFind::with_size(3);
+        let mut uf = super::UnionFind::with_size(3, |_| ());
         uf.union(0, 1);
         uf.union(1, 2);
 
-        assert_eq!(uf.parents[0].get(), 1);
+        match &uf.contents[0] {
+            crate::union_find::UnionFindNode::Data(_) => panic!("Variant not expected"),
+            crate::union_find::UnionFindNode::Pointer(cell) => assert_eq!(cell.get(), 1),
+        }
+
         uf.find(0);
-        assert_eq!(uf.parents[0].get(), 2);
+
+        match &uf.contents[0] {
+            crate::union_find::UnionFindNode::Data(_) => panic!("Variant not expected"),
+            crate::union_find::UnionFindNode::Pointer(cell) => assert_eq!(cell.get(), 2),
+        }
     }
 
     #[test]
     fn union_find_user() {
-        let uf = RefCell::new(super::UnionFind::with_size(3));
-        let set_0 = super::Set::new(&uf);
-        let set_1 = super::Set::new(&uf);
-        let set_2 = super::Set::new(&uf);
+        let uf = Rc::new(RefCell::new(super::UnionFind::default()));
+        let set_0 = super::Set::new(&uf, ());
+        let set_1 = super::Set::new(&uf, ());
+        let set_2 = super::Set::new(&uf, ());
 
         let set_01 = set_0.clone().union(set_1.clone());
 
@@ -168,7 +213,7 @@ mod tests {
         assert_ne!(set_0, set_2);
         assert_ne!(set_1, set_2);
 
-        let set_3 = super::Set::new(&uf);
+        let set_3 = super::Set::new(&uf, ());
 
         let set_31 = set_3.clone().union(set_1.clone());
 
