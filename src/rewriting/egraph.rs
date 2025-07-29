50ca1747d7bf0550e415b3acee5d8eec428c0161
Szymon Zyguła <szymon@zygula.eu>
2025-07-29 14:16:28 +0200

    Add working class merging and rebuilding to e-graphs

--- src/rewriting/egraph.rs
+++ src/rewriting/egraph.rs
@@ -1,5 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
+use itertools::Itertools;
+
 use crate::{
     language::{
         expression::{Literal, VarFreeExpression},
@@ -43,6 +45,14 @@ impl Node {
             *child_id = graph.canonical_class(*child_id);
         }
     }
+
+    /// Assume `self` is a symbol and return it, otherwise panic.
+    fn as_symbol(&self) -> &Symbol<ClassId> {
+        match self {
+            Node::Literal(literal) => panic!("Expected node to be a symbol, is {:?}", literal),
+            Node::Symbol(symbol) => symbol,
+        }
+    }
 }
 
 #[derive(Clone, Debug, Default)]
@@ -65,7 +75,8 @@ impl Class {
     }
 }
 
-// TODO: rebuilding after class merging
+// TODO: matching (bottom up for now?)
+// TODO: rule application
 #[derive(Default)]
 struct EGraph {
     union_find: UnionFind,
@@ -126,6 +137,7 @@ impl EGraph {
     /// If a given node exists in the e-graph, returns it. Otherwise gives `None`.
     fn node_id(&self, node: &Node) -> Option<NodeId> {
         // TODO: Add hashcons so that this is speedy
+        // Are hashcons really that much better when they hold uncanonical, potentially outdated keys anyway...?
         let node = node.canonical(self);
 
         for (&candidate_id, candidate_node) in self.nodes.iter() {
@@ -160,12 +172,25 @@ impl EGraph {
         &self.nodes[&node_id]
     }
 
+    pub fn node_mut(&mut self, node_id: NodeId) -> &mut Node {
+        self.nodes.get_mut(&node_id).unwrap()
+    }
+
     pub fn class_count(&self) -> usize {
         self.classes.len()
     }
 
-    pub fn node_count(&self) -> usize {
+    /// Returns the total number of nodes which were added to the egraph at any point
+    pub fn total_node_count(&self) -> usize {
         self.nodes.len()
+    }
+
+    /// Returns the current number of nodes in the egraph
+    pub fn actual_node_count(&self) -> usize {
+        self.classes
+            .iter()
+            .map(|(_, class)| class.nodes_ids.len())
+            .sum()
     }
 
     pub fn parents(&self, class_id: ClassId) -> &HashSet<NodeId> {
@@ -190,6 +215,76 @@ impl EGraph {
 
         let class_1 = self.classes.remove(&class_1_id).unwrap();
         self.classes.get_mut(&class_2_id).unwrap().merge(class_1);
+
+        self.rebuild_class(class_2_id);
+    }
+
+    /// Makes all nodes in an eclass have canonical children
+    fn make_class_canonical(&mut self, class_id: ClassId) {
+        let class_id = self.canonical_class(class_id);
+        let class = &self.classes[&class_id];
+        for &node_id in &class.nodes_ids {
+            // Make node canonical
+            for child_id in self.nodes.get_mut(&node_id).unwrap().iter_mut_children() {
+                *child_id = self.union_find.find(*child_id);
+            }
+        }
+    }
+
+    fn rebuild_class(&mut self, class_id: ClassId) {
+        // Canonicalize nodes in this class
+        self.make_class_canonical(class_id);
+
+        let class = &self.classes[&class_id];
+
+        // Find duplicate nodes
+        let mut to_remove = Vec::new();
+        for (&node_id_1, &node_id_2) in class
+            .nodes_ids
+            .iter()
+            .cartesian_product(class.nodes_ids.iter())
+            .filter(|(id_1, id_2)| id_1 < id_2)
+        {
+            if self.node(node_id_1) == self.node(node_id_2) {
+                to_remove.push(node_id_1);
+            }
+        }
+
+        // Remove duplicate nodes
+        let class = self.classes.get_mut(&class_id).unwrap();
+        for node_id in to_remove {
+            class.nodes_ids.remove(&node_id);
+        }
+
+        // Make all nodes in parents have canonical class IDs
+        let class = &self.classes[&class_id];
+        for parent in &class.parents_ids {
+            for child in self.nodes.get_mut(&parent).unwrap().iter_mut_children() {
+                *child = self.union_find.find(*child);
+            }
+        }
+
+        // Check if there are parents who share identical nodes
+        let mut to_merge = Vec::new();
+        for (parent_1_id, parent_2_id) in class
+            .parents_ids
+            .iter()
+            .cartesian_product(class.parents_ids.iter())
+            // So that we don't compare same things twice
+            .filter(|(id_1, id_2)| id_1 < id_2)
+        {
+            let class_1_id = self.containing_class(*parent_1_id);
+            let class_2_id = self.containing_class(*parent_2_id);
+
+            if class_1_id != class_2_id && self.nodes[parent_1_id] == self.nodes[parent_2_id] {
+                to_merge.push((class_1_id, class_2_id));
+            }
+        }
+
+        // Merge parents with identical nodes
+        for (class_1_id, class_2_id) in to_merge {
+            self.merge_classes(class_1_id, class_2_id);
+        }
     }
 }
 
@@ -216,7 +311,8 @@ mod tests {
         let graph = EGraph::from_expression(expression);
 
         assert_eq!(graph.class_count(), 7);
-        assert_eq!(graph.node_count(), 7);
+        assert_eq!(graph.total_node_count(), 7);
+        assert_eq!(graph.actual_node_count(), 7);
     }
 
     #[test]
@@ -231,7 +327,8 @@ mod tests {
         let node_2_id = graph.add_expression(expression_2);
 
         assert_eq!(graph.class_count(), 6);
-        assert_eq!(graph.node_count(), 6);
+        assert_eq!(graph.total_node_count(), 6);
+        assert_eq!(graph.actual_node_count(), 6);
 
         let class_1_id = graph.containing_class(node_1_id);
         let class_2_id = graph.containing_class(node_2_id);
@@ -243,7 +340,8 @@ mod tests {
 
         assert_eq!(class_1_id, class_2_id);
         assert_eq!(graph.class_count(), 5);
-        assert_eq!(graph.node_count(), 6);
+        assert_eq!(graph.total_node_count(), 6);
+        assert_eq!(graph.actual_node_count(), 6);
     }
 
     #[test]
@@ -322,5 +420,69 @@ mod tests {
 
         assert_eq!(egraph.node_id(&symbol_1), egraph.node_id(&symbol_2));
         assert_eq!(node_3_id, node_4_id);
+    }
+
+    #[test]
+    fn literal_sharing() {
+        let mut egraph = EGraph::default();
+
+        let lang = Language::math();
+        let expr_1: VarFreeExpression = lang.parse_no_vars("(+ 1 5)").unwrap();
+        let expr_2: VarFreeExpression = lang.parse_no_vars("(* 6 5)").unwrap();
+
+        egraph.add_expression(expr_1);
+        egraph.add_expression(expr_2);
+
+        assert_eq!(egraph.actual_node_count(), 5);
+        assert_eq!(egraph.total_node_count(), 5);
+        assert_eq!(egraph.class_count(), 5);
+    }
+
+    #[test]
+    fn class_merge_with_same_nodes() {
+        let mut egraph = EGraph::default();
+
+        let lang = Language::math();
+        let expr_1: VarFreeExpression = lang.parse_no_vars("(+ 1 5)").unwrap();
+        let expr_2: VarFreeExpression = lang.parse_no_vars("(+ 1 2)").unwrap();
+
+        let expr_1_id = egraph.add_expression(expr_1);
+        let expr_2_id = egraph.add_expression(expr_2);
+
+        let class_5 = egraph.node(expr_1_id).as_symbol().children[1];
+        let class_2 = egraph.node(expr_2_id).as_symbol().children[1];
+
+        egraph.merge_classes(class_5, class_2);
+
+        assert_eq!(egraph.total_node_count(), 5);
+        assert_eq!(egraph.class_count(), 3);
+        assert_eq!(egraph.actual_node_count(), 4);
+    }
+
+    #[test]
+    fn cascading_rebuild() {
+        let lang = Language::math();
+
+        let expr = lang
+            .parse_no_vars("(* (+ 5 (sin (* 1 7))) (+ 5 (sin (* 1 8))))")
+            .unwrap();
+
+        let mut egraph = EGraph::from_expression(expr);
+
+        let id_7 = egraph.node_id(&Node::Literal(Literal::Int(7))).unwrap();
+        let id_8 = egraph.node_id(&Node::Literal(Literal::Int(8))).unwrap();
+
+        let id_7_class = egraph.containing_class(id_7);
+        let id_8_class = egraph.containing_class(id_8);
+
+        assert_eq!(egraph.total_node_count(), 11);
+        assert_eq!(egraph.actual_node_count(), 11);
+        assert_eq!(egraph.class_count(), 11);
+
+        egraph.merge_classes(id_7_class, id_8_class);
+
+        assert_eq!(egraph.total_node_count(), 11);
+        assert_eq!(egraph.actual_node_count(), 8);
+        assert_eq!(egraph.class_count(), 7);
     }
 }
