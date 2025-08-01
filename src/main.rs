716fbef56fbf174ee6568c5e34323992af369c20
Szymon Zyguła <szymon@zygula.eu>
2025-08-01 15:48:35 +0200

    Add some code to main which saves a dot image of an e-graph

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #![allow(dead_code)]
 
+use language::Language;
+use rewriting::{
+    egraph::{EGraph, matching::bottom_up::BottomUpMatcher, saturation::saturate},
+    rule::Rule,
+};
+
 mod data_union_find;
 mod index_selector;
 mod language;
@@ -7,4 +13,18 @@ mod rewriting;
 mod seen;
 mod union_find;
 
-fn main() {}
+fn main() {
+    let lang = Language::math();
+    let rules = vec![
+        Rule::from_strings("(* x0 2)", "(<< x0 1)", &lang),
+        Rule::from_strings("(* x0 1)", "x0", &lang),
+        Rule::from_strings("(/ (* x0 x1) x2)", "(* x0 (/ x1 x2))", &lang),
+        Rule::from_strings("(/ x0 x0)", "1", &lang),
+    ];
+
+    let mut egraph = EGraph::from_expression(lang.parse_no_vars("(/ (* (sin 5) 2) 2)").unwrap());
+
+    saturate(&mut egraph, &rules, BottomUpMatcher);
+
+    egraph.save_dot(&lang, "test.dot").unwrap();
+}
