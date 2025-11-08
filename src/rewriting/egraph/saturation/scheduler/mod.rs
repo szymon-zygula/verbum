d10029236ed62076c2e43e9d8966da87243ada58
Szymon Zyguła <szymon@zygula.eu>
2025-11-08 18:58:24 +0100

    Fix a typo

--- src/rewriting/egraph/saturation/scheduler/mod.rs
+++ src/rewriting/egraph/saturation/scheduler/mod.rs
@@ -2,7 +2,7 @@ use crate::rewriting::egraph::matching::Matcher;
 use crate::rewriting::egraph::{Analysis, EGraph};
 
 /// The `Scheduler` trait defines a strategy for choosing which rule to try next
-/// and applying it using the provided matcher. It returns the number of
+/// and applies it using the provided matcher. It returns the number of
 /// applications performed in this step (0 means no rule applied, i.e., saturated).
 pub trait Scheduler<A: Analysis> {
     fn apply_next(&mut self, egraph: &mut EGraph<A>, matcher: &dyn Matcher) -> usize;
